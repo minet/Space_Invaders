02572fea62477013cf48fe1e3bb9543b351115da
Artlef <artlef@minet.net>
2015-09-28 18:47:00 +0200

    IA collision

--- src/collidesystem.cpp
+++ src/collidesystem.cpp
@@ -8,36 +8,40 @@ void CollideSystem::run() {
             World::world.hasComponents[i][AABB] &&
             World::world.hasComponents[i][POSITION]) {
 
-            if (World::world.hasComponents[i][INPUT] &&
-                World::world.aabbs[i].x < 0) {
+            if (World::world.hasComponents[i][INPUT]) {
 
-               World::world.aabbs[i].x = 0;
-               World::world.positions[i].x = 0;
+              if (World::world.aabbs[i].x < 0) {
 
+                  World::world.aabbs[i].x = 0;
+                  World::world.positions[i].x = 0;
+               }
+
+               if (World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w) {
+
+                  World::world.aabbs[i].x = window.getSize().x - World::world.aabbs[i].w;
+                  World::world.positions[i].x = window.getSize().x - World::world.aabbs[i].w;
+               }
             }
 
-            if (World::world.hasComponents[i][INPUT] &&
-                World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w) {
 
-               World::world.aabbs[i].x = window.getSize().x - World::world.aabbs[i].w;
-               World::world.positions[i].x = window.getSize().x - World::world.aabbs[i].w;
+            if (World::world.hasComponents[i][IA]) {
 
+                if ((World::world.aabbs[i].x < 0) ||
+                    (World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w)) {
+
+                    for (auto j = 0u; j < World::world.numberEntities;j++) {
+
+                       if (World::world.hasComponents[j][IA] && World::world.hasComponents[j][VELOCITY]){
+                             World::world.velocities[j].x *= -1;
+                       }
+                    }
+                }
             }
 
-            if (World::world.hasComponents[i][IA] &&
-                World::world.aabbs[i].x < 0) {
+            for (j = 0u; j< World::world.numberEntities;j++) {
+                if (j!=i && ) {
 
-               World::world.aabbs[i].x = 0;
-               World::world.positions[i].x = 0;
-
-            }
-
-            if (World::world.hasComponents[i][IA] &&
-                World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w) {
-
-               World::world.aabbs[i].x = window.getSize().x - World::world.aabbs[i].w;
-               World::world.positions[i].x = window.getSize().x - World::world.aabbs[i].w;
-
+                }
             }
         }
     }
