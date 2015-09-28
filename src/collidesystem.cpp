78952ef04d152a992e7dc1d34f0cd716d81e68c5
Artlef <artlef@orange.fr>
2015-09-28 18:08:55 +0200

    collidesystem3

--- src/collidesystem.cpp
+++ src/collidesystem.cpp
@@ -8,14 +8,32 @@ void CollideSystem::run() {
             World::world.hasComponents[i][AABB] &&
             World::world.hasComponents[i][POSITION]) {
 
-            if (World::world.aabbs[i].x < 0) {
+            if (World::world.hasComponents[i][INPUT] &&
+                World::world.aabbs[i].x < 0) {
 
                World::world.aabbs[i].x = 0;
                World::world.positions[i].x = 0;
 
             }
 
-            if (World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w) {
+            if (World::world.hasComponents[i][INPUT] &&
+                World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w) {
+
+               World::world.aabbs[i].x = window.getSize().x - World::world.aabbs[i].w;
+               World::world.positions[i].x = window.getSize().x - World::world.aabbs[i].w;
+
+            }
+
+            if (World::world.hasComponents[i][IA] &&
+                World::world.aabbs[i].x < 0) {
+
+               World::world.aabbs[i].x = 0;
+               World::world.positions[i].x = 0;
+
+            }
+
+            if (World::world.hasComponents[i][IA] &&
+                World::world.aabbs[i].x > window.getSize().x - World::world.aabbs[i].w) {
 
                World::world.aabbs[i].x = window.getSize().x - World::world.aabbs[i].w;
                World::world.positions[i].x = window.getSize().x - World::world.aabbs[i].w;
